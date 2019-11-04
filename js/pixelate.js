0735ae0a3d7cd6ddd70e9a4ba959c6bb26f4c619
Colin Pelkmans <colin.pelkmans@gmail.com>
2019-11-04 11:59:55 +0100

    Changed colors

--- js/pixelate.js
+++ js/pixelate.js
@@ -6,20 +6,21 @@ let controller = new BleController();
 document.addEventListener('DOMContentLoaded', (event) => {
     document.getElementById("connect").addEventListener("click", () => {
         controller.connect()
-        .then(() => {
-            console.log("Connected to BLE controller");
-            cursor.change_color({r:0, g:0, b:200});
-            controller.addEventListener('touch', (event) => {
-                switch(event.touchedKey) {
-                    case 'R': cursor.move_right(); break;
-                    case 'L': cursor.move_left(); break;
-                    case 'U': cursor.move_up(); break;
-                    case 'D': cursor.move_down(); break;
-                    case 'X': cursor.colorize({r: 200, g:0, b:0}); break;
-                }
+            .then(() => {
+                console.log("Connected to BLE controller");
+                controller.addEventListener('touch', (event) => {
+                    switch (event.touchedKey) {
+                        case 'R': cursor.move_right(); break;
+                        case 'L': cursor.move_left(); break;
+                        case 'U': cursor.move_up(); break;
+                        case 'D': cursor.move_down(); break;
+                        case 'X': cursor.colorize({ r: 100, g: 0, b: 0 }); break;
+                        case 'A': cursor.colorize({ r: 0, g: 100, b: 0 }); break;
+                        case 'B': cursor.colorize({ r: 0, g: 0, b: 100 }); break;
+                    }
+                });
+            }).catch((error) => {
+                console.log(error);
             });
-        }).catch((error) => {
-            console.log(error);
-        });
     });
 });
